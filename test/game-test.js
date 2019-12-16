8706f9e46c2fcc9de161a9928d82611d7944b181
caleb.haizlett@gmail.com <alanturing@example.c>
2019-12-16 13:44:40 -0700

    Add basic boilerplate for each class test

---
+++ test/game-test.js
@@ -0,0 +1,10 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('Game', () => {
+  it('should be a function', function() {
+  expect(Game).to.be.a('function');
+});
+})
+
+export default Game;
