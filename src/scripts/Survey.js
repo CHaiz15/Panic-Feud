355cc002b5e2a000ea1657f84210ab70ff7988f3
caleb.haizlett@gmail.com <alanturing@example.c>
2019-12-17 14:12:00 -0700

    Add survey class testing of defaults and methods

--- src/scripts/Survey.js
+++ src/scripts/Survey.js
@@ -1,7 +1,19 @@
 class Survey {
-  constructor() {
-    
+  constructor(data) {
+    this.id = data.surveys[0].id;
+    this.question = data.surveys[0].question;
+    this.answers = data.answers;
+  }
+  checkGuess(playerGuess) {
+    return this.answers.reduce((acc, answer) => {
+      acc.push(answer.answer.toLowerCase());
+      return acc;
+    }, []).includes(playerGuess.toLowerCase())
+  }
+  findPoints(playerGuess) {
+    let matchingAnswer = this.answers.find(answer => answer.answer === playerGuess);
+    return matchingAnswer.respondents;
   }
 }
 
-export default Survey
+export default Survey;
--- test/survey-test.js
+++ test/survey-test.js
@@ -1,10 +1,60 @@
 import chai from 'chai';
 const expect = chai.expect;
 
-describe('Survey', () => {
-  it('should be a function', function() {
-  expect(Survey).to.be.a('function');
-});
-})
+import Survey from '../src/scripts/Survey.js';
 
-export default Survey;
+describe('Survey:', () => {
+  let survey;
+  let data = {
+    surveys: [{
+      id: 1,
+      question: 'If You Drew Homer Simpson’s Name In A Secret Santa Exchange, What Would You Buy Him?'
+    }, ],
+    answers: [{
+        answer: 'Beer',
+        respondents: 67,
+        surveyId: 1
+      },
+      {
+        answer: 'Bowling Ball',
+        respondents: 5,
+        surveyId: 1
+      },
+      {
+        answer: 'Donuts',
+        respondents: 24,
+        surveyId: 1
+      },
+    ]
+  };
+  let playerGuess;
+  beforeEach(() => {
+    survey = new Survey(data);
+  });
+  it('should be a function', function() {
+    expect(Survey).to.be.a('function');
+  });
+  describe('Default Values:', () => {
+    it('should have a survey id', function() {
+      expect(survey.id).to.equal(1);
+    });
+    it('should have a survey question', function() {
+      expect(survey.question).to.equal('If You Drew Homer Simpson’s Name In A Secret Santa Exchange, What Would You Buy Him?');
+    });
+    it('should have an array of answers', function() {
+      expect(survey.answers).to.equal(data.answers)
+    });
+    describe('Method Values:', () => {
+      it('should return true/flase whether the players guess is included', function() {
+        playerGuess = 'Beer';
+        expect(survey.checkGuess(playerGuess)).to.equal(true);
+        playerGuess = 'A dog';
+        expect(survey.checkGuess(playerGuess)).to.equal(false);
+      });
+      it('should return the number of associated respondents to the answer', function() {
+        playerGuess = 'Beer';
+        expect(survey.findPoints(playerGuess)).to.equal(67);
+      });
+    });
+  });
+});
