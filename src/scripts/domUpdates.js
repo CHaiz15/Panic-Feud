7065ac8c1b7d772a85584b056b4242d339a065cf
Grayson Palmer <gpalmer1385@gmail.com>
2019-12-19 16:35:57 -0700

    Add file for DOM manipulation and button/screen transitions

---
+++ src/scripts/domUpdates.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+
+$('.prepare-self').click(function() {
+  $('.main-menu').fadeOut(500);
+  $('.instructions')
+    .delay(500)
+    .css("display", "flex")
+    .hide()
+    .fadeIn(500);
+  });
+
+$('.lets-begin').click(function() {
+  $('.instructions').fadeOut(500);
+  $('.game-screen')
+    .delay(500)
+    .css("display", "grid")
+    .hide()
+    .fadeIn(500);
+});
