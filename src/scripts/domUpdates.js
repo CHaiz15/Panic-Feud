20b3a4493c20cd81096ec35a7ff9cbab3f6346c2
Grayson Palmer <gpalmer1385@gmail.com>
2020-01-06 14:10:02 -0700

    Begin functionrs for interpolation for game screen

--- src/scripts/domUpdates.js
+++ src/scripts/domUpdates.js
@@ -1,5 +1,7 @@
 import $ from 'jquery';
 
+
+
 $('.prepare-self').click(function() {
   $('.main-menu').fadeOut(500);
   $('.instructions')
@@ -18,3 +20,37 @@ $('.lets-begin').click(function() {
     .fadeIn(500);
 });
 
+const populateSurveyAndAnswers = (round) => {
+  $('.survey').html(`
+  <h2 class="survey-question">${round.survey}</h2>
+    <div class="survey-answer-wrapper">
+      <div class="survey-answer answer1">
+        <h3>${round.answers[0].answer}</h3>
+        <p>${round.answers[0].respondents}</p>
+      </div>
+      <div class="survey-answer answer2">
+        <h3>${round.answers[1].answer}</h3>
+        <p>${round.answers[1].respondents}</p>
+      </div>
+      <div class="survey-answer answer3">
+        <h3>${round.answers[2].answer}</h3>
+        <p>${round.answers[2].respondents}</p>
+      </div>
+    </div>
+  `)
+}
+
+const populatePlayerInformation = (player) => {
+  $('player1-screen').html(`
+    <h3>${player.name}</h3>
+    <p>Total Score: 0</p>
+    <p>Round Score: 0</p>
+    <form>
+      <input class="player1-answer-input" type="text">
+    </form>
+    <div class="player1-multiplier multiplier">Multiplier Selector</div>
+    <p>Incorrect guesses:</p>
+    <div class="player1-wrong-guesses wrong-guesses">
+    </div>
+  `)
+}
